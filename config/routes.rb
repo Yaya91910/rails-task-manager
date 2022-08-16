1d65cc51a8b0a0f91c6eee3252798e3ac659c16e
Yassin Fekih <yassin.fekih@free.fr>
2022-08-16 15:47:42 +0200

    exo en cours

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/tasks/new', to: 'tasks#new'
+
+  post '/tasks', to: 'tasks#create'
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
+
+  get '/tasks', to: 'tasks#index'
+
+  get '/tasks/:id', to: 'tasks#show', as: :task
 end
